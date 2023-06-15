4f3f4b29be377b5a026c17f349022e554f7b4a30
Medi, Erim <erim.medi@haw-hamburg.de>
2023-06-15 12:21:52 +0200

    added tailwind

--- bachelor/src/frontend/src/App.vue
+++ bachelor/src/frontend/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
+    <Navbar/>
     <RouterView/>
   </div>
 </template>
 
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
+import Navbar from './components/Navbar.vue';
 
 </script>
 
