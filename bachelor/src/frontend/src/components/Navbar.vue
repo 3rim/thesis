eaf434287d470538002a063d03f3e3aa0f634124
Medi, Erim <erim.medi@haw-hamburg.de>
2023-06-15 14:31:16 +0200

    implementing: navbar

--- bachelor/src/frontend/src/components/Navbar.vue
+++ bachelor/src/frontend/src/components/Navbar.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>
-        <h1>hello world</h1>
-    </div>
+    <header>
+        <nav class="container lg:inline-flex ">
+            <p>Meine Medien</p>
+            <p>Ausleihe/Rücknahme</p>
+            <p>Inventar</p>
+            <ul class="list-style none">
+                <li>Aufnehmen</li>
+                <li>Löschen</li>
+            </ul>
+            <p>User</p>
+            <ul>
+                <li>Neu </li>
+                <li>Löschen</li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script setup lang="ts">
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
