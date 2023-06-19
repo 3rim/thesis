cff6d7fa8587e390a95c71fbe1c0fff606367f08
Medi, Erim <erim.medi@haw-hamburg.de>
2023-06-19 15:39:10 +0200

    asyncBorrowerView

---
+++ bachelor/src/frontend/src/components/AsyncBorrowerView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-col flex-1 items-center">
+        <!-- User  -->
+        <div>
+            <p class="py-1 text-white font-bold"> {{borrowerData.data.firstName}} {{borrowerData.data.lastName}}</p>
+        </div>
+
+        <!-- Medialist-->
+        <div></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios' ;
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+const getBorrowerData =async () => {
+    try {
+        const borrowerData = await axios.get(
+            `/api/v1/user/${route.query.id}`
+        );
+        console.log(borrowerData);
+        return borrowerData;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const borrowerData = await getBorrowerData();    
+console.log(borrowerData);
+
+
+</script>
+
--- bachelor/src/frontend/src/views/LendView.vue
+++ bachelor/src/frontend/src/views/LendView.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-
+    <div class="container">
+        <div class=" pt-4 mb-8 ">
+             <input 
+        type="text" 
+        placeholder="Mediencode einscannen oder tippen"
+        v-model="searchQuery"
+        @input="getSearchResults()"
+        class="py-2 px-1 w-full bg-transparent border-b
+         focus:border-b-gray-600 
+         focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"/>
+        </div>
+       
+        <Suspense>
+        <AsyncBorrowerView />
+        <template #fallback>
+            <p>loadning...</p>
+        </template>
+        </Suspense>
     </div>
 </template>
 
 <script setup lang="ts">
+import AsyncBorrowerView from '../components/AsyncBorrowerView.vue';
 
 </script>
-
-<style scoped>
-
-</style>
