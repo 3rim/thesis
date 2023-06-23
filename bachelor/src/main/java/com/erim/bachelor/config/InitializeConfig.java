3f842da62e4426cd03adb82bb8cc551032547684
3rim <erim_medi@hotmail.de>
2023-06-23 18:46:42 +0200

    add simple errorMessage

--- bachelor/src/main/java/com/erim/bachelor/config/InitializeConfig.java
+++ bachelor/src/main/java/com/erim/bachelor/config/InitializeConfig.java
@@ -21,10 +21,10 @@ public class InitializeConfig {
         return args -> {
             ArrayList<Medium> media = new ArrayList<>(
                     Arrays.asList(
-                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).build(),
-                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).build(),
-                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).build(),
-                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).build(),
+                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).ISBN("51651651").build(),
+                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).ISBN("51651651").build(),
+                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).ISBN("51651651").build(),
+                            Medium.builder().title("Java ist auch eine Insel").status(Status.AVAILABLE).ISBN("51651651").build(),
 
                             Medium.builder().title("IPad 8.Gen").status(Status.AVAILABLE).serialNr("F9FFABCDQ1GC").build(),
                             Medium.builder().title("IPad 8.Gen").status(Status.AVAILABLE).serialNr("F8FFABCDQ1GC").build(),
