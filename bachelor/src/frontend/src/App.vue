5d0d2987fdb3c765355e74957a5593f7657fcf3b
3rim <erim_medi@hotmail.de>
2023-08-22 21:00:37 +0200

    autoLogout and redirect to login page when jwt expires

--- bachelor/src/frontend/src/App.vue
+++ bachelor/src/frontend/src/App.vue
@@ -8,12 +8,28 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue'
-import { computed} from 'vue';
+import { computed, watch } from 'vue';
 import { useStore } from 'vuex';
+import {useRouter} from 'vue-router'
 
 const store = useStore();
+const router = useRouter();
 
 const loggedIn = computed(() =>{
     return store.state.auth.status.loggedIn;
 })
+
+const autoLogout = computed(() =>{
+    return store.state.auth.status.autoLogout;
+})
+
+//Watch if autoLogout was triggerd => redirect to LoginPage
+watch(autoLogout, (newValue) => {
+  console.log(autoLogout+`x is ${newValue}` )
+  router.push("/login")
+})
+
+
+
+
 </script>
--- bachelor/src/frontend/src/store/auth.module.js
+++ bachelor/src/frontend/src/store/auth.module.js
@@ -1,15 +1,19 @@
 import AuthService from '../services/authService';
+import VueJwtDecode from 'vue-jwt-decode'
+import jwt_decode from 'jwt-decode';
+import { faL } from '@fortawesome/free-solid-svg-icons';
+
 
 const user = JSON.parse(localStorage.getItem('user'));
 const initialState = user
-  ? { status: { loggedIn: true }, user }
-  : { status: { loggedIn: false }, user: null };
+  ? { status: { loggedIn: true,autoLogout:false }, user }
+  : { status: { loggedIn: false ,autoLogout:true}, user: null };
 
 export const auth = {
   namespaced: true,
   state: initialState,
   actions: {
-    login({ commit }, user) {
+    login({ commit,dispatch}, user) {
       return AuthService.login(user).then(
         user => {
           if(user.initialLogin){
@@ -19,6 +23,14 @@ export const auth = {
           }
           else{
             commit('loginSuccess', user);
+
+            const decoded =jwt_decode(user.jwt)
+            let expirationTime = decoded.exp *1000;
+            console.log(expirationTime);
+            setTimeout(()=>{
+              dispatch("auto_logout")
+            },3*1000)
+  
             return Promise.resolve(user);
           }
           
@@ -30,13 +42,19 @@ export const auth = {
       );
     },
     logout({ commit }) {
+      console.log("logout")
       AuthService.logout();
       commit('logout');
+    },
+    auto_logout({commit,dispatch}){
+      commit('autoLogout')
+      dispatch('logout');
     }
   },
   mutations: {
     loginSuccess(state, user) {
       state.status.loggedIn = true;
+      state.status.autoLogout = false;
       state.user = user;
     },
     loginFailure(state) {
@@ -45,6 +63,11 @@ export const auth = {
     },
     logout(state) {
       state.status.loggedIn = false;
+      state.user = null;
+    },
+    autoLogout(state,user){
+      state.status.loggedIn = false;
+      state.status.autoLogout = true;
       state.user = null;
     }
   }
