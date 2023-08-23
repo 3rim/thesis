dad6cc417e63e3a5815ed8a8fce70a64e1e5e52a
3rim <erim_medi@hotmail.de>
2023-08-23 18:19:27 +0200

    added vee-validate for form validation

--- bachelor/src/frontend/src/views/LoginView.vue
+++ bachelor/src/frontend/src/views/LoginView.vue
@@ -3,65 +3,75 @@
         <div class="mt-20 bg-gray-300 px-2 py-2 rounded-lg shadow-lg "
         v-if="!showInitLogin">
             <p class="text-center font-bold">Login</p>
-            <form @submit.prevent="handleLogin">
-                <!--Email-->
-                <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
-                    Email
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="email" type="text" placeholder="email"
-                v-model="email" required>
-                <!--password-->
-                <div v-if="!showInitLogin">
-                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
-                        Passwort
+            
+            <Form 
+                @submit="handleLogin" 
+                
+                class=""
+                >
+                <div>
+                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+                        Email
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="password" type="text" placeholder="password"
-                    v-model="password" required>
+                    <Field v-model="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="email" type="email" />
+                    <ErrorMessage class="text-red-500" name="email" />
                 </div>
-                <!--Init login-->
-                <div v-if="showInitLogin">
+                <div>
+                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <Field class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="password" type="password" />
+                    
+                </div>
+                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Login</button>
+            </Form>
+        </div>
+        <div class="mt-20 bg-gray-300 px-2 py-2 rounded-lg shadow-lg "
+        v-else>
+            <p class="text-center font-bold">Neues Passwort setzen</p>            
+            <Form 
+                @submit="submitPasswort" 
+                :validation-schema="initLoginSchema"
+                class=""
+                >
+                <div>
+                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <Field v-model="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="email" type="email" />
+                    <ErrorMessage class="text-red-500" name="email" />
+                </div>
+                <div>
+                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="currentPassword">
+                        Aktuelles Passwort
+                    </label>
+                    <Field v-model="currentPassword" class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="currentPassword" type="password" />
+                    <ErrorMessage class="text-red-500" name="currentPassword" />
+                </div>
+                <div>
                     <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="newPassword">
                         Neues Passwort
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="password" type="text" placeholder="newPassword"
-                    v-model="newPassword" required>
-                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="repeatPassword">
-                        Wiederhole Passwort
-                    </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="password" type="text" placeholder="repeatPassword"
-                    v-model="repeatPassword" required>
+                    <Field class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="newPassword" type="password" />
+                    <!--<ErrorMessage class="text-red-500" name="newPassword" />-->
+  
+                    <ErrorMessage  name="newPassword">
+                     <p class="text-red-500"> Passwort muss min. 8 Zeichen lang sein</p>
+                    </ErrorMessage>
                 </div>
-                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Login</button>
-            </form>
-
-            <form>
-
-            </form>
-        </div>
-
-        <div class="mt-20 bg-gray-300 px-2 py-2 rounded-lg shadow-lg "
-        v-else>
-            <p class="text-center font-bold">Neues Passwort setzen</p>
-            <form @submit.prevent="submitPasswort">
-                <!--Email-->
-                <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
-                    Email
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="email" type="text" placeholder="email"
-                v-model="email" required>
-                <!--password-->
-                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
-                       Aktuelles Passwort
+                <div>
+                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="passwordConfirmation">
+                        Passwort wiederholen
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="password" type="text" placeholder="password"
-                    v-model="password" required>  
-                    <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="newPassword">
-                       Neues Passwort
-                    </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="newPassword" type="text" placeholder="Neues Passwort"
-                    v-model="newPassword" required>              
-                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">speichern</button>
-            </form>
+                    <Field class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="passwordConfirmation" type="password" />
+                    <ErrorMessage  name="passwordConfirmation">
+                     <p class="text-red-500"> Passwort stimmt nicht überein</p>
+                    </ErrorMessage>
+                </div>
+                
+                
+                <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Passwort setzen</button>
+            </Form>
         </div>
 
         <div v-if="message"
@@ -72,17 +82,32 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 import axios from "axios";
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
-import { onMounted } from 'vue';
+import {Form, Field,ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+const schema = yup.object({
+  //email: yup.string().required().email(),
+  //name: yup.string().required(),
+  //password: yup.string().required().min(8),
+});
+const initLoginSchema = yup.object({
+  email: yup.string().required().email(),
+  newPassword: yup.string().required().min(8),
+  passwordConfirmation: yup
+    .string()
+    .required()
+    .oneOf([yup.ref('newPassword')], 'Passwords do not match'),
+});
 
 const router = useRouter();
 const email = ref();
 const password = ref();
 const newPassword = ref();
-const repeatPassword = ref();
+const currentPassword = ref();
 const loading = ref(false);
 const store = useStore();
 const API_URL = '/api/v1/auth/';
@@ -90,20 +115,28 @@ const API_URL = '/api/v1/auth/';
 const message = ref(null);
 const showInitLogin = ref(false);
 
-const submitPasswort = async () =>{
+
+function onSubmit(values) {
+  // Submit values to API...
+  console.log(values)
+  alert(JSON.stringify(values, null, 2));
+}
+const submitPasswort = async (form) =>{
+    console.log(form)
     let user ={
         email: email.value,
         currentPassword: password.value,
         newPassword: newPassword.value
     }
 
-    const response = await axios.post(API_URL + 'initialLogin', user)
+    const response = await axios.post(API_URL + 'initialLogin', form)
     router.go(0);
 }
 
 
 //dispatch 'auth/login' Action to Vuex Store. If the login is successful, go to Profile Page, otherwise, show error message.
-const handleLogin = async () =>{
+const handleLogin = async (form) =>{
+    console.log("lasds")
     loading.value = true;
     let user ={
         email: email.value,
@@ -114,7 +147,7 @@ const handleLogin = async () =>{
     
     
         //login
-        store.dispatch("auth/login",user).then(
+        store.dispatch("auth/login",form).then(
         (response) => {
             if(response.initialLogin){
                 showInitLogin.value = true;
