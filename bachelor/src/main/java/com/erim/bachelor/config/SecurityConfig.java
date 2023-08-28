a9406514c79c64bc0e7b3e1f372d55ee4b89ddc5
3rim <erim_medi@hotmail.de>
2023-08-28 18:14:09 +0200

    added SecurityFilterConfig for the FilterChain removed spring-security stuff from InitConfig to SecurityConfig. application.yml : added logging for spring-security

--- bachelor/src/main/java/com/erim/bachelor/config/SecurityConfig.java
+++ bachelor/src/main/java/com/erim/bachelor/config/SecurityConfig.java
@@ -1,76 +1,42 @@
 package com.erim.bachelor.config;
 
-import com.erim.bachelor.enums.Role;
-import com.erim.bachelor.security.JwtAuthenticationFilter;
+import com.erim.bachelor.repositories.BorrowerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity // Enable @PreAuthorize at Method-Level
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-
-    private final String ADMIN = Role.ADMIN.toString();
-    private final String LIBRARIAN = Role.LIBRARIAN.toString();
-    private final String LOAN_HELPER = Role.LOAN_HELPER.toString();
-    private final String INVENTORY_HELPER = Role.INVENTORY_HELPER.toString();
-
-    private final String[] AUTH_WHITE_LIST ={
-            // -- Swagger UI v2
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            // -- Swagger UI v3 (OpenAPI)
-            "/v3/api-docs/**",
-            "/swagger-ui/**",
-            //API Authentication
-            "/api/v1/auth/**",
-            //Error Page ResponseStatusException always redirect to /error page which request authentication due to the latest spring version
-            "/error"
-
-    };
+    private final BorrowerRepository repository;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests()
-                //Public API
-                .requestMatchers(AUTH_WHITE_LIST)
-                    .permitAll()
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findBorrowerByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 
-                //Configure Borrower-Admin endpoints
-                .requestMatchers(HttpMethod.POST, "/api/v1/borrowers/**").hasAuthority(ADMIN)
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/borrowers/**").hasAuthority(ADMIN)
-                .requestMatchers(HttpMethod.PUT, "/api/v1/borrowers/**").hasAuthority(ADMIN)
-                //Configure Inventory endpoints
-                .requestMatchers( "/api/v1/inventory/**").hasAnyAuthority(ADMIN,LIBRARIAN,INVENTORY_HELPER)
-                .requestMatchers( "/api/v1/loan/**").hasAnyAuthority(ADMIN,LIBRARIAN,LOAN_HELPER)
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,PasswordEncoder passwordEncoder){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return  authProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
---
+++ bachelor/src/main/java/com/erim/bachelor/config/SecurityFilterConfig.java
@@ -0,0 +1,76 @@
+package com.erim.bachelor.config;
+
+import com.erim.bachelor.enums.Role;
+import com.erim.bachelor.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity(debug = true)
+@EnableMethodSecurity // Enable @PreAuthorize at Method-Level
+public class SecurityFilterConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final String ADMIN = Role.ADMIN.toString();
+    private final String LIBRARIAN = Role.LIBRARIAN.toString();
+    private final String LOAN_HELPER = Role.LOAN_HELPER.toString();
+    private final String INVENTORY_HELPER = Role.INVENTORY_HELPER.toString();
+
+    private final String[] AUTH_WHITE_LIST ={
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            //API Authentication
+            "/api/v1/auth/**",
+            //Error Page ResponseStatusException always redirect to /error page which request authentication due to the latest spring version
+            "/error"
+
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                //Public API
+                .requestMatchers(AUTH_WHITE_LIST)
+                .permitAll()
+
+                //Configure Borrower-Admin endpoints
+                .requestMatchers(HttpMethod.POST, "/api/v1/borrowers/**").hasAuthority(ADMIN)
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/borrowers/**").hasAuthority(ADMIN)
+                .requestMatchers(HttpMethod.PUT, "/api/v1/borrowers/**").hasAuthority(ADMIN)
+                //Configure Inventory endpoints
+                .requestMatchers( "/api/v1/inventory/**").hasAnyAuthority(ADMIN,LIBRARIAN,INVENTORY_HELPER)
+                .requestMatchers( "/api/v1/loan/**").hasAnyAuthority(ADMIN,LIBRARIAN,LOAN_HELPER)
+
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
