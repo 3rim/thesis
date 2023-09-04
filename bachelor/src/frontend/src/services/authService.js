8de6ccedaee9587e670e2a96c6a79da2a3b484e9
3rim <erim_medi@hotmail.de>
2023-09-04 17:33:51 +0200

    delete comment

--- bachelor/src/frontend/src/services/authService.js
+++ bachelor/src/frontend/src/services/authService.js
@@ -9,22 +9,9 @@ class AuthService {
       .post(API_URL + 'login', user)
       .then(response => {
         if (response.data.jwt) {
-          
-         /*
-         //unparse payload
-          var base64Payload = response.data.jwt.split(".")[1];
-          var payload = decodeURIComponent(
-            atob(base64Payload)
-              .split("")
-              .map(function (c) {
-                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-              })
-              .join("")
-          );
-          console.log(JSON.parse(payload));*/
           localStorage.setItem('user', JSON.stringify(response.data));
         }
-
+        console.log(response)
         return response.data;
       });
   }
